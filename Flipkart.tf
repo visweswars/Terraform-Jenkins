f953ae1430b5ac800b1c55b983e1689903481d62
visweswars <84311353+visweswars@users.noreply.github.com>
2023-09-11 11:18:37 +0530

    Update Flipkart.tf

--- Flipkart.tf
+++ Flipkart.tf
@@ -1,9 +1,13 @@
 provider "aws" {
-region = "ap-south-1"
-access_key = "AKIAYRG7AKZBMMKURXES"
-secret_key = "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"
+    region = "ap-south-1"
 }
-resource  "aws_s3_bucket" "dataamazon987simplesweb" {
-bucket = "dataamazon987simplesweb"
-acl = "private"
+
+resource "aws_s3_bucket" "b" {
+    bucket = "myawsbucket123455544"
+    acl = "private"
+
+    tags = {
+        Name = "My bucket"
+        Environment = "DevJKT"
+    }
 }
