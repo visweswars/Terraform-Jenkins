b293213cc8e1388d2e4881870c7a59fad70774c5
visweswars <84311353+visweswars@users.noreply.github.com>
2023-11-28 17:25:22 +0530

    Update Flipkart.tf

--- Flipkart.tf
+++ Flipkart.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-region = "ap-south-1"
-access_key = "AKIAYRG7AKZBMMKURXES"
-secret_key = "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"
+region = "us-east-1"
+access_key = "AKIAWZWF7QOD5WZMZY6N"
+secret_key = "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"
 }
-resource "aws_instance" "testdev" {
-ami = "ami-02bb7d8191b50f4bb"
+resource "aws_instance" "testserver" {
+ami = "ami-0fc5d935ebf8bc3bc"
 instance_type = "t2.micro"
-availability_zone = "ap-south-1a"
-key_name = "testdev"
+availability_zone = "us-east-1c"
+key_name = "testserver"
 tags = {
         Name = "data"
 }
